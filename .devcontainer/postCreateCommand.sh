26fc08249c4fc687f8586ec56aa433c9deb1ff7e
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-01-07 12:37:19 +0100

    chore: init 🚀

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -ex
+
+# wait for docker to be ready
+wait_for_docker() {
+  while true; do
+    docker ps > /dev/null 2>&1 && break
+    sleep 1
+  done
+  echo "Docker is ready."
+}
+
+wait_for_docker
+
+# This file is called in three scenarios:
+# 1. fresh creation of devcontainer
+# 2. rebuild
+# 3. full rebuild
+
+# download images beforehand, optional
+ddev debug download-images
+
+# avoid errors on rebuilds
+ddev poweroff
+
+# start ddev project automatically
+# ddev start -y
+
+# further automated install / setup steps, e.g. 
+# ddev composer install 
+# See e.g. https://github.com/mandrasch/ddev-craftcms-vite/blob/main/.devcontainer/postCreateCommand.sh
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # launch-codespaces-with-ddev
 Launch a fresh blank Codespaces instance with DDEV installed
+
+Based on https://ddev.readthedocs.io/en/latest/users/install/ddev-installation/#github-codespaces
