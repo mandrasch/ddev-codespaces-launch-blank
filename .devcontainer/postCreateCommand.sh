80fbaf713264fdb7390ac298a56f01b27e19940f
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2024-06-05 16:24:31 +0200

    chore: try modified wait for docker (WIP)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,15 +2,28 @@
 set -ex
 
 # wait for docker to be ready
-wait_for_docker() {
-  while true; do
-    docker ps > /dev/null 2>&1 && break
-    sleep 1
-  done
-  echo "Docker is ready."
-}
+#wait_for_docker() {
+#  while true; do
+#    docker ps > /dev/null 2>&1 && break
+#    sleep 1
+#  done
+#  echo "Docker is ready."
+#}
+# wait_for_docker
 
-wait_for_docker
+# Docker can take a couple seconds to come up. Wait for it to be ready before
+# proceeding with bootstrap. https://github.com/devcontainers/features/issues/977#issuecomment-2148230117
+iterations=10
+while ! docker ps &>/dev/null; do
+  if [[ $iterations -eq 0 ]]; then
+    echo "Timeout waiting for the Docker daemon to start."
+    exit 1
+  fi
+
+  iterations=$((iterations - 1))
+  echo 'Docker is not ready. Waiting 10 seconds and trying again.'
+  sleep 10
+done
 
 # This file is called in three scenarios:
 # 1. fresh creation of devcontainer
